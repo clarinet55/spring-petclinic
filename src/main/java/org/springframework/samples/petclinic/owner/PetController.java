e5378ead8fdd1ee2221af72d68904fafc13cf9fa
clari <clarinet55@gmail.com>
2024-11-04 22:37:26 +0900

    hogehoge

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -15,26 +15,20 @@
  */
 package org.springframework.samples.petclinic.owner;
 
-import java.time.LocalDate;
-import java.util.Collection;
-
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
 /**
- * @author Juergen Hoeller
+ * @author Juergen Hoeller hogehoge
  * @author Ken Krebs
  * @author Arjen Poutsma
  */
